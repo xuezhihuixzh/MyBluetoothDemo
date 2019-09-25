c4a0b581dec6b720a19e3a6f25b9ae519ca8c7fc
xuezhihui <2531295581@qq.com>
2019-09-25 17:00:08 +0800

    蓝牙

---
+++ MyBluetoothDemo/app/src/main/java/com/demo/mybluetoothdemo/callback/MyBleCallBack.java
@@ -0,0 +1,11 @@
+package com.demo.mybluetoothdemo.callback;
+
+import android.bluetooth.BluetoothDevice;
+
+/**
+ * Created by admin on 2017/1/9.
+ */
+public interface MyBleCallBack{
+    public void callbleBack(BluetoothDevice device);
+
+}
